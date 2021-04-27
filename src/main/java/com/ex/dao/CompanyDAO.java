00137b3634304d457bd98c13efdb08c4e90b3547
IonTsoku <toncuion99@gmail.com>
2021-04-27 19:29:21 +0300

    Create DAO interface for company and implemented this in CompanyService

--- src/main/java/com/ex/dao/CompanyDAO.java
+++ src/main/java/com/ex/dao/CompanyDAO.java
@@ -1,7 +1,18 @@
 package com.ex.dao;
+
+import com.ex.entities.actors.Company;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /*
  * @IonToncu
  *
  * */
 public interface CompanyDAO {
+    void addDesigner(Company company)throws SQLException;
+    List<Company> getAll()throws SQLException;
+    Company getById(long id)throws SQLException;
+    void update(Company company)throws SQLException;
+    void remove(Company company)throws SQLException;
 }
