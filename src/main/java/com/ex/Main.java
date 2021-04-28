f770c3b301c2edb2a84f06ccc1cf4c33110fbac2
IonTsoku <toncuion99@gmail.com>
2021-04-28 12:06:58 +0300

    change dependency, and config file

--- src/main/java/com/ex/Main.java
+++ src/main/java/com/ex/Main.java
@@ -1,16 +1,14 @@
 package com.ex;
 
 import com.ex.bl.SessionUtil;
+
 //import com.ex.config.SpringConfig;
 
 import com.ex.entities.actors.Customer;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /*
  * @IonToncu
@@ -19,57 +17,17 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        SessionUtil sessionUtil=new SessionUtil();
+       SessionUtil sessionUtil=new SessionUtil();
+       sessionUtil.openTransactionSession();
 
-//        sessionUtil.openTransactionSession();
-//        Session session= sessionUtil.getSession();
-////
-//        Customer customer=new Customer();
-//        customer.setName("Customer 1");
-//
-//
-//
-////        session.save(customer);
-//        sessionUtil.closeTransactionSession();
-//        //context.close();
-        sessionUtil.openTransactionSession();
-      Session session=sessionUtil.getSession();
-//        Customer c= new Customer();
-//        c.setName("customer1");
-//        session.save(c);
-//        for(int i=0;i<10;i++){
-//            A a=new A();
-//            a.setName("a=efgsege");
-//            session.save(a);
-//        }
-        String sql ="select * from a";
+        Session session=sessionUtil.getSession();
+        String sql="select * from a";
         Query query=session.createNativeQuery(sql).addEntity(A.class);
         List<A> aList=query.list();
         for(A a:aList) System.out.println(a);
-//
-//                String sql="SELECT * FROM customer";
-//                Session session1=sessionUtil.getSession();
-//                Query query =session1.createNativeQuery(sql).addEntity(Customer.class);
-//                List<Customer> customerList=query.list();
-//                for(Customer c:customerList)
 
-//                String sql="select * from customer";
-//                Session session=sessionUtil.getSession();
-//                Query query=session.createNativeQuery(sql).addEntity(Customer.class);
-//                List<Customer>customerList=query.list();
-//                System.out.println(customerList);
-            //openTransactionSession();
-//            long id=1;
-//            Session session=sessionUtil.getSession();
-//            String sql="SELECT * FROM customer WHERE ID =:id";
-//            Query query=session.createNativeQuery(sql).addEntity(Customer.class);
-//            query.setParameter("id",id);
-//            Customer customer=(Customer)query.getSingleResult();
-//            System.out.println(customer.getName());
-//            //closeTransactionSession();
-                sessionUtil.closeTransactionSession();
-        System.out.println("ceva");
+        sessionUtil.closeTransactionSession();
+        System.out.println("done");
 
 
     }
